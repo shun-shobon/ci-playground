45a4a01fd9ff609ec2585980bcf88538d274c5c8
Shuntaro Nishizawa <me@shun.technology>
2023-03-18 03:16:51 +0900

    chore(mod.ts): add import for copy function from fs module

--- mod.ts
+++ mod.ts
@@ -1,6 +1,9 @@
 export * from "https://deno.land/std@0.179.0/testing/asserts.ts";
 import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
-import * as fs from "https://deno.land/std@0.170.0/fs/mod.ts";
+import* as fs from "https://deno.land/std@0.170.0/fs/mod.ts";
+import {
+  copy,
+} from "https://deno.land/std@0.170.0/fs/mod.ts";
 import{
 Cookie,
   ConnInfo,
