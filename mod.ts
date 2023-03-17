b93a46c7e2c84fd36eeacd5743d2e83c25c783c3
Shuntaro Nishizawa <me@shun.technology>
2023-03-18 02:42:37 +0900

    chore: add Deno and VSCode settings, and renovate.json file with regex manager for Deno dependencies

---
+++ mod.ts
@@ -0,0 +1,11 @@
+export * from "https://deno.land/std@0.179.0/testing/asserts.ts";
+import { build, emptyDir } from "https://deno.land/x/dnt@0.33.0/mod.ts";
+import * as fs from "https://deno.land/std@0.170.0/fs/mod.ts";
+import{
+Cookie,
+  ConnInfo,
+      ClientErrorStatus,
+  CookieMap,
+CookieMapOptions,
+  CookieMapSetDeleteOptions
+}             from"https://deno.land/std@0.180.0/http/mod.ts"     ;
